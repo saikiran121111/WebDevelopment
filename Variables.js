2685c3663c3099be0d3258d677db145e846a4928
saikiran121111 <saikiranvsk3@gmail.com>
2025-01-22 13:43:30 +0530

    first chage

---
+++ Variables.js
@@ -0,0 +1,9 @@
+let num = 2+2 // let is used to declare a variable at first in js which is different in other languages like java
+//where in java  we use int a = 5; something like that
+console.log(num)
+num = 6 // here I'm not using the let because num is already declared I'm just using that to change the value
+console.log(num)// you can rewrite the value of num aswell
+console.log(num+3)
+let username = "Ruku"
+let userid = 'Ruku' // string can be kept either in single quotes or in double quotes
+console.log(username,userid)
